ccee5e5ff07e93a4d54c3691f3b83fd52e88969a
Abby Schroeder <abigail.r.schroeder@gmail.com>
2010-04-27 13:03:24 -0500

    Added apoem.rb

---
+++ apoem.rb
@@ -0,0 +1,20 @@
+Shoes.app :width => 358, :height => 450 do
+  flow :width => 358, :margin => 15 do
+    stack :width => "100%" do
+      banner "NIK NAMES"
+    end
+    stack :width  => "90px" do
+      para "Cato potato,\n",
+           "catie potatie"
+    end
+    stack :width => "-90px" do
+      para "abby doodle,\n" ,
+           "abby de\n\n" ,
+           "silly jilly,\n" ,
+           "jilly bean\n\n" ,
+           "all the nick names\n" ,
+           "i have heard\n" ,
+           "are absurd!"
+   end
+  end
+end
